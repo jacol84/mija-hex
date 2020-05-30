0a06a091aa5bfe9247d08e0c410d74a5c6d47c4d
jac <devjac@ogarnij.se>
2020-05-30 17:26:21 +0200

    mija-hex-12  OrderMapper.kt i OrderMapperImpl.kt przeniesione do factory zamienione na fukcje rozszerzającą dany obiekt

--- src/main/kotlin/mija/hex/domain/order/Order.kt
+++ src/main/kotlin/mija/hex/domain/order/Order.kt
@@ -1,24 +1,34 @@
 package mija.hex.domain.order
 
+import mija.hex.domain.order.port.shared.OrderDto
 import mija.hex.domain.order.port.shared.OrderState
+import java.util.concurrent.atomic.AtomicInteger
 
 data class Order internal constructor(val orderId: Int, val disName: String, val state: OrderState)
-
 
 object OrderFactory {
 
     private val SEQUENCE = AtomicInteger()
-    private val orderMapper: OrderMapper = OrderMapperImpl()
+
 
     fun createOrder(disName: String): Order {
         return Order(SEQUENCE.incrementAndGet(), disName, OrderState.NEW)
     }
 
     fun from(orderDto: OrderDto): Order {
-        return orderMapper.toOrder(orderDto)
+        return orderDto.toOrder()
     }
 
-    fun toOrderDto(order: Order): OrderDto {
-        return orderMapper.toOrderDto(order)
-    }
+    fun toOrderDto(order: Order) = order.toOrderDto()
+
+    fun toOrder(orderDto: OrderDto) = orderDto.toOrder()
+
+}
+
+private fun OrderDto.toOrder(): Order {
+    return Order(orderId, dishName, state)
+}
+
+private fun Order.toOrderDto(): OrderDto {
+    return OrderDto(orderId, disName, state)
 }
--- src/main/kotlin/mija/hex/domain/order/OrderMapper.kt
+++
@@ -1,8 +0,0 @@
-package mija.hex.domain.order
-
-import mija.hex.domain.order.port.shared.OrderDto
-
-internal interface OrderMapper {
-    fun toOrderDto(order: Order): OrderDto
-    fun toOrder(orderDto: OrderDto): Order
-}
