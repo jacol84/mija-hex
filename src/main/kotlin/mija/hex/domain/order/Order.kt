6ab8bcc9c51701cf68a19933dfc7d1e96e586d7f
jac <devjac@ogarnij.se>
2020-05-30 15:07:40 +0200

    mija-hex-9  OrderFactory to Order

--- src/main/kotlin/mija/hex/domain/order/Order.kt
+++ src/main/kotlin/mija/hex/domain/order/Order.kt
@@ -1,6 +1,26 @@
 package mija.hex.domain.order
 
+import mija.hex.domain.order.port.shared.OrderDto
 import mija.hex.domain.order.port.shared.OrderState
+import java.util.concurrent.atomic.AtomicInteger
 
-data class Order(val orderId: Int, val disName: String, val state: OrderState)
+data class Order internal constructor(val orderId: Int, val disName: String, val state: OrderState)
 // TODO public only getter
+
+object OrderFactory {
+
+    private val SEQUENCE = AtomicInteger()
+    private val orderMapper: OrderMapper = OrderMapperImpl()
+
+    fun createOrder(disName: String): Order {
+        return Order(SEQUENCE.incrementAndGet(), disName, OrderState.NEW)
+    }
+
+    fun from(orderDto: OrderDto): Order {
+        return orderMapper.toOrder(orderDto)
+    }
+
+    fun toOrderDto(order: Order): OrderDto {
+        return orderMapper.toOrderDto(order)
+    }
+}
