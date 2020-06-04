4800dcdb1c2aa3aa9c33933af0be47965f46a38c
jac <devjac@ogarnij.se>
2020-06-04 21:02:36 +0200

    mija-hex-26 part #4.0  remove DRY for test

--- src/test/kotlin/mija/hex/domain/order/DomainTest2.kt
+++ src/test/kotlin/mija/hex/domain/order/DomainTest2.kt
@@ -1,45 +1,16 @@
 package mija.hex.domain.order
 
-import mija.hex.domain.order.port.secondary.Logistics
-import mija.hex.domain.order.port.secondary.OrderStore
-import mija.hex.domain.order.port.shared.OrderDto
 import mija.hex.domain.order.port.shared.OrderState
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class DomainTest2 {
-    private val oderStore: OrderStore = object : OrderStore {
-        private val memory: MutableMap<Int, OrderDto> = mutableMapOf()
-        override fun save(dto: OrderDto) {
-            println("OrderStore::save: ${dto.orderId}")
-            memory[dto.orderId] = dto
-        }
-
-        override fun load(id: Int): OrderDto? {
-            println("OrderStore::load: $id")
-            return memory[id]
-        }
-
-        override fun findByState(state: OrderState): Collection<OrderDto> {
-            println("OrderStore::findByState: $state")
-            return memory.filter { it.value.state == state }.values
-        }
-    }
-    private val logistics: Logistics = object : Logistics {
-        override fun prepareOrder(orderId: Int) {
-            TODO("Not yet implemented")
-        }
-
-        override fun deliver(orderId: Int) {
-            TODO("Not yet implemented")
-        }
-    }
 
     @Test
     fun testNewBurger() {
-        val orderFacade = OrderFacade(oderStore, logistics)
+        val orderFacade = OrderFacade(createOrderStoreForTest(), createEmptyLogistic())
         val orderId = orderFacade.foodOrderService.createOrder("Burger", "ul. Balonowa")
-//        assertEquals(1, orderId)
+//        assertEquals(1, orderId) // FIXME how restart seq
         val orderState = orderFacade.foodOrderService.getOrderState(orderId)
         assertEquals(OrderState.NEW, orderState)
     }
--- src/test/kotlin/mija/hex/domain/order/DomainTestV2.kt
+++ src/test/kotlin/mija/hex/domain/order/DomainTestV2.kt
@@ -1,9 +1,6 @@
 package mija.hex.domain.order
 
 import mija.hex.domain.order.port.primary.FoodOrderService
-import mija.hex.domain.order.port.secondary.Logistics
-import mija.hex.domain.order.port.secondary.OrderStore
-import mija.hex.domain.order.port.shared.OrderDto
 import mija.hex.domain.order.port.shared.OrderState
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -11,37 +8,11 @@ import kotlin.test.assertNotNull
 import kotlin.test.assertNull
 
 class DomainTestV2 {
-    private val oderStore: OrderStore = object : OrderStore {
-        private val memory: MutableMap<Int, OrderDto> = mutableMapOf()
-        override fun save(dto: OrderDto) {
-            println("OrderStore::save: ${dto.orderId}")
-            memory[dto.orderId] = dto
-        }
-
-        override fun load(id: Int): OrderDto? {
-            println("OrderStore::load: $id")
-            return memory[id]
-        }
-
-        override fun findByState(state: OrderState): Collection<OrderDto> {
-            println("OrderStore::findByState: $state")
-            return memory.filter { it.value.state == state }.values
-        }
-    }
-    private val logistics: Logistics = object : Logistics {
-        override fun prepareOrder(orderId: Int) {
-            TODO("Not yet implemented")
-        }
-
-        override fun deliver(orderId: Int) {
-            TODO("Not yet implemented")
-        }
-    }
 
     @Test
     fun `simple domain test - use kotlin test - check status`() {
         //given
-        val orderFacade = OrderFacade(oderStore, logistics)
+        val orderFacade = OrderFacade(createOrderStoreForTest(), createEmptyLogistic())
         val foodOrderService: FoodOrderService = orderFacade.foodOrderService
         //given
         val orderId: Int = foodOrderService.createOrder("Burger", "ul. Balonowa")
@@ -53,14 +24,15 @@ class DomainTestV2 {
     @Test
     fun `simple domain test - use kotlin test - check exists in store and name is burger`() {
         //given
-        val orderFacade = OrderFacade(oderStore, logistics)
+        val orderStore = createOrderStoreForTest()
+        val orderFacade = OrderFacade(orderStore, createEmptyLogistic())
         val foodOrderService: FoodOrderService = orderFacade.foodOrderService
         //given
         val orderId: Int = foodOrderService.createOrder("Burger", "ul. Balonowa")
         //then
         val orderState: OrderState? = foodOrderService.getOrderState(orderId)
         assertEquals(OrderState.NEW, orderState)
-        val dto = oderStore.load(orderId)
+        val dto = orderStore.load(orderId)
         assertNotNull(dto)
         assertEquals("Burger", dto.disName)
     }
@@ -68,7 +40,7 @@ class DomainTestV2 {
     @Test
     fun `simple domain test - use kotlin test - check status when id not exists in store`() {
         //given
-        val orderFacade = OrderFacade(oderStore, logistics)
+        val orderFacade = OrderFacade(createOrderStoreForTest(), createEmptyLogistic())
         val foodOrderService: FoodOrderService = orderFacade.foodOrderService
         //given
         val orderId: Int = -15
